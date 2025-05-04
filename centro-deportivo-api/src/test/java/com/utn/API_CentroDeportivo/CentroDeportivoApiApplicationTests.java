d300ecd5c6e00ba299e2d683f7ba3d5c9bbe5634
Matias Benedetti <benedettimaty@gmail.com>
2025-05-04 16:49:40 -0300

    agrega proyecto creado con spring initializr

---
+++ centro-deportivo-api/src/test/java/com/utn/API_CentroDeportivo/CentroDeportivoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.API_CentroDeportivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroDeportivoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
