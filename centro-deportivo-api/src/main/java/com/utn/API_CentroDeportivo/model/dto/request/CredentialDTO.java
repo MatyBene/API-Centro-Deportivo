5e3143b7f324a353d99fa37e3f09080802673e9b
Matias Benedetti <benedettimaty@gmail.com>
2025-05-18 13:53:07 -0300

    modifica validaciones de CredentialDTO

--- centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/model/dto/request/CredentialDTO.java
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/model/dto/request/CredentialDTO.java
@@ -3,6 +3,7 @@ package com.utn.API_CentroDeportivo.model.dto.request;
 import com.utn.API_CentroDeportivo.model.entity.User;
 import com.utn.API_CentroDeportivo.model.enums.Role;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -16,7 +17,12 @@ import lombok.experimental.SuperBuilder;
 public class CredentialDTO {
     @NotBlank(message = "El nombre de usuario es obligatorio")
     private String username;
-    @NotBlank(message = "La contraseña es obligatoria")
+
+    @NotBlank(message = "La contraseña no puede estar vacía")
     @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$",
+            message = "La contraseña debe contener al menos: 1 mayúscula, 1 minúscula, 1 número, 1 carácter especial y no tener espacios"
+    )
     private String password;
 }
