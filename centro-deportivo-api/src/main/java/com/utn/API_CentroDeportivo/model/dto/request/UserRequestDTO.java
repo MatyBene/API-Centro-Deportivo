b4d12c73ae9f4a682deedc0dcf011ff6ecd8bdf7
Matias Benedetti <benedettimaty@gmail.com>
2025-05-18 13:50:54 -0300

    modifica validaciones para user

--- centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/model/dto/request/UserRequestDTO.java
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/model/dto/request/UserRequestDTO.java
@@ -1,9 +1,6 @@
 package com.utn.API_CentroDeportivo.model.dto.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -16,16 +13,22 @@ import lombok.experimental.SuperBuilder;
 public class UserRequestDTO {
     @NotBlank(message = "El nombre es obligatorio")
     private String name;
+
     @NotBlank(message = "El apellido es obligatorio")
     private String lastname;
+
     @NotBlank(message = "El dni es obligatorio")
-    @Min(value = 1000000, message = "El DNI debe tener al menos 7 dígitos")
+    @Pattern(regexp = "\\d{8}", message = "El DNI debe tener al menos 7 dígitos")
     private String dni;
+
     @NotBlank(message = "La fecha de nacimiento es obligatoria")
-    @Past(message = "La fecha de nacimiento debe ser en el pasado")
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "El formato de la fecha de nacimiento debe ser YYYY-MM-DD")
     private String birthdate;
+
     @NotBlank(message = "El teléfono es obligatorio")
+    @Size(max = 15, message = "El teléfono no puede tener más de 15 dígitos")
     private String phone;
+
     @NotBlank(message = "El email es obligatorio")
     @Email(message = "El email debe tener un formato válido")
     private String email;
