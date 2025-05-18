0aad92d060b931bd0f514133ae4d2f4cc6739b29
Matias Benedetti <benedettimaty@gmail.com>
2025-05-18 13:45:07 -0300

    crea ErrorResponseDTO

---
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/config/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.utn.API_CentroDeportivo.config;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
