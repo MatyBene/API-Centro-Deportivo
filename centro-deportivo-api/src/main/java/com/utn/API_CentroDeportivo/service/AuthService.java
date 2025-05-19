329c7da635d0dd2ddf33ecfd80061034494eb858
Matias Benedetti <benedettimaty@gmail.com>
2025-05-19 12:14:28 -0300

    modifica registerMember

--- centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/service/AuthService.java
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/service/AuthService.java
@@ -4,6 +4,7 @@ import com.utn.API_CentroDeportivo.model.dto.request.CredentialRequestDTO;
 import com.utn.API_CentroDeportivo.model.dto.request.UserRequestDTO;
 import com.utn.API_CentroDeportivo.model.entity.Credential;
 import com.utn.API_CentroDeportivo.model.entity.Member;
+import com.utn.API_CentroDeportivo.model.entity.User;
 import com.utn.API_CentroDeportivo.model.enums.Role;
 import com.utn.API_CentroDeportivo.model.enums.Status;
 import com.utn.API_CentroDeportivo.model.exception.FieldAlreadyExistsException;
@@ -26,22 +27,33 @@ public class AuthService implements IAuthService {
 
     @Transactional
     public void registerMember(UserRequestDTO memberDTO) {
-        if (userRepository.existsByDni(memberDTO.getDni())){
-            throw new FieldAlreadyExistsException("El DNI ya está registrado");
-        }
-        if (userRepository.existsByEmail(memberDTO.getEmail())) {
-            throw new FieldAlreadyExistsException("El email ya está registrado");
-        }
-        if (credentialRepository.existsByUsername(memberDTO.getUsername())) {
-            throw new FieldAlreadyExistsException("El nombre de usuario ya está registrado");
-        }
+        validateUserFields(memberDTO);
         Member member = MemberMapper.mapToMember(memberDTO);
-        CredentialRequestDTO credentialDTO = CredentialRequestDTO.builder().username(memberDTO.getUsername()).password(memberDTO.getPassword()).build();
-        Credential credential = CredentialMapper.mapToCredential(credentialDTO, member);
-        credential.setRole(Role.MEMBER);
-        member.setCredential(credential);
         member.setStatus(Status.INACTIVE);
-        userRepository.save(member);
+        createAndSaveUser(member, Role.MEMBER);
+    }
+
+    private void validateUserFields(UserRequestDTO userDTO) {
+        if (userRepository.existsByDni(userDTO.getDni())) {
+            throw new FieldAlreadyExistsException("El DNI ya está registrado.");
+        }
+        if (userRepository.existsByEmail(userDTO.getEmail())) {
+            throw new FieldAlreadyExistsException("El email ya está registrado.");
+        }
+        if (credentialRepository.existsByUsername(userDTO.getUsername())) {
+            throw new FieldAlreadyExistsException("El nombre de usuario ya está registrado.");
+        }
+    }
+
+    private void createAndSaveUser(User user, Role role) {
+        CredentialRequestDTO credentialDTO = CredentialRequestDTO.builder()
+                .username(user.getCredential().getUsername())
+                .password(user.getCredential().getPassword())
+                .build();
+        Credential credential = CredentialMapper.mapToCredential(credentialDTO, user);
+        credential.setRole(role);
+        user.setCredential(credential);
+        userRepository.save(user);
     }
 
 }
