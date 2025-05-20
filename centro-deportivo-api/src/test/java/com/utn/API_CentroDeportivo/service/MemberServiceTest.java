c17ddfed2c7ca45b359997e4364c040d79102581
Matias Benedetti <benedettimaty@gmail.com>
2025-05-19 22:11:58 -0300

    crea MemberServiceTest

---
+++ centro-deportivo-api/src/test/java/com/utn/API_CentroDeportivo/service/MemberServiceTest.java
@@ -0,0 +1,12 @@
+package com.utn.API_CentroDeportivo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @Test
+    void updateMemberStatus() {
+    }
+}
