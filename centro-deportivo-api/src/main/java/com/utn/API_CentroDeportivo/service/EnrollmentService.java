e4e21024c75ce5a21473c93babc1e7a551a79f6d
Matias Benedetti <benedettimaty@gmail.com>
2025-06-02 14:36:31 -0300

    edita enrollMemberToActivity en EnrollmentService

--- centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/service/EnrollmentService.java
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/service/EnrollmentService.java
@@ -6,35 +6,32 @@ import com.utn.API_CentroDeportivo.model.entity.Member;
 import com.utn.API_CentroDeportivo.model.entity.SportActivity;
 import com.utn.API_CentroDeportivo.model.exception.MemberAlreadyEnrolledException;
 import com.utn.API_CentroDeportivo.model.repository.IEnrollmentRepository;
-import com.utn.API_CentroDeportivo.model.repository.ISportActivityRepository;
-import com.utn.API_CentroDeportivo.model.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class EnrollmentService implements IEnrollmentService{
 
     @Autowired
-    private IUserRepository userRepository;
-
-    @Autowired
-    private ISportActivityRepository sportActivityRepository;
-
-    @Autowired
     private IEnrollmentRepository enrollmentRepository;
+
+    @Autowired
+    private IMemberService memberService;
+
+    @Autowired
+    private ISportActivityService sportActivityService;
 
     @Transactional
     public void enrollMemberToActivity(EnrollmentRequestDTO enrollmentDTO) {
         Long memberId = enrollmentDTO.getMemberId();
         Long activityId = enrollmentDTO.getActivityId();
 
-        Member member = (Member) userRepository.findById(memberId)
-                .orElseThrow(() -> new RuntimeException("Socio no encontrado"));
-        SportActivity activity = sportActivityRepository.findById(activityId)
-                .orElseThrow(() -> new RuntimeException("Actividad no encontrada"));
+        Member member = memberService.getMemberById(memberId).get();
+        SportActivity activity = sportActivityService.getSportActivityById(activityId).get();
 
         if (enrollmentRepository.findByMemberIdAndActivityId(memberId, activityId).isPresent()) {
             throw new MemberAlreadyEnrolledException("El socio ya está inscripto en esta actividad");
