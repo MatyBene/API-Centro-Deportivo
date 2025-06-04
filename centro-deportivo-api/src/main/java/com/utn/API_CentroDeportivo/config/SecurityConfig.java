d6e459dc50651a56d2980420ddcf2db1455c6d5e
Matias Benedetti <benedettimaty@gmail.com>
2025-06-04 12:59:00 -0300

    crea metodo authenticationProvider en SecurityConfig

--- centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/config/SecurityConfig.java
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.utn.API_CentroDeportivo.config;
 
+import com.utn.API_CentroDeportivo.service.CredentialService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,5 +18,13 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(CredentialService credentialDetails){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(credentialDetails);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 }
