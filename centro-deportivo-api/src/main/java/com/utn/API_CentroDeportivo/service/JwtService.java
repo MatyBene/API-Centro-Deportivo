3b145f8658963e1622bf4d9370ee5c516c142989
Matias Benedetti <benedettimaty@gmail.com>
2025-06-04 12:44:43 -0300

    crea metodo generateToken en JwtService

---
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/service/JwtService.java
@@ -0,0 +1,52 @@
+package com.utn.API_CentroDeportivo.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtService implements IJwtService{
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Override
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .claim("rol", userDetails.getAuthorities())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    @Override
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        return false;
+    }
+
+    @Override
+    public String extractUsername(String token) {
+        return "";
+    }
+
+    @Override
+    public boolean isTokenExpired(String token) {
+        return false;
+    }
+
+    @Override
+    public Claims extractAllClaims(String token) {
+        return null;
+    }
+
+    @Override
+    public Key getKey() {
+        return null;
+    }
+}
