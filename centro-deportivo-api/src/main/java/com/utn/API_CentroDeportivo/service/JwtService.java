aa7c57807db71fd91b0adb67866a85534e27e3cf
Matias Benedetti <benedettimaty@gmail.com>
2025-06-04 12:57:57 -0300

    crea metodo passwordEncoder en SecurityConfig

--- centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/service/JwtService.java
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/service/JwtService.java
@@ -6,11 +6,13 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
+@Service
 public class JwtService implements IJwtService{
 
     @Value("${jwt.secret}")
