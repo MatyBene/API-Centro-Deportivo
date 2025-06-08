9c61307a8cbae7fbc4b02f832a9296af6366d7b9
TomasB <tomasbservant@gmail.com>
2025-06-08 18:18:08 -0300

    incluye metodo unsubscribeMemberFromActivity en enrollmentService

--- centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/service/EnrollmentService.java
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/service/EnrollmentService.java
@@ -3,8 +3,10 @@ package com.utn.API_CentroDeportivo.service;
 import com.utn.API_CentroDeportivo.model.entity.Enrollment;
 import com.utn.API_CentroDeportivo.model.entity.Member;
 import com.utn.API_CentroDeportivo.model.entity.SportActivity;
+import com.utn.API_CentroDeportivo.model.enums.Status;
 import com.utn.API_CentroDeportivo.model.exception.MemberAlreadyEnrolledException;
 import com.utn.API_CentroDeportivo.model.repository.IEnrollmentRepository;
+import com.utn.API_CentroDeportivo.model.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,6 +28,9 @@ public class EnrollmentService implements IEnrollmentService{
     @Autowired
     private ISportActivityService sportActivityService;
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @Transactional
     public void enrollMemberToActivity(String username, Long activityId) {
 
@@ -45,5 +50,21 @@ public class EnrollmentService implements IEnrollmentService{
 
         enrollmentRepository.save(enrollment);
         memberService.updateMemberStatus(member.getId());
+    }
+    @Transactional
+    @Override
+    public void unsubscribeMemberFromActivity(String username, Long activityId) {
+        Member member = (Member) credentialService.getUserByUsername(username);
+
+        Enrollment enrollment = enrollmentRepository.findByMemberIdAndActivityId(member.getId(), activityId)
+                .orElseThrow(() -> new IllegalStateException("El socio no está inscripto en esta actividad"));
+
+        enrollmentRepository.delete(enrollment);
+
+        boolean hasOtherEnrollments = enrollmentRepository.existsByMemberId(member.getId());
+        if (!hasOtherEnrollments) {
+            member.setStatus(Status.INACTIVE);
+            userRepository.save(member);
+        }
     }
 }
