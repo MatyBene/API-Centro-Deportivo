dd903790161e17ac5a74b81d75c381efc73cac04
TomasB <tomasbservant@gmail.com>
2025-06-08 17:44:14 -0300

    agrega metodo getMyActivityDetails en instructorController

--- centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/controller/InstructorController.java
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/controller/InstructorController.java
@@ -3,9 +3,11 @@ package com.utn.API_CentroDeportivo.controller;
 import com.utn.API_CentroDeportivo.model.dto.response.InstructorDetailsDTO;
 import com.utn.API_CentroDeportivo.model.dto.response.InstructorSummaryDTO;
 import com.utn.API_CentroDeportivo.model.dto.response.SportActivityDetailsDTO;
+import com.utn.API_CentroDeportivo.model.entity.Instructor;
 import com.utn.API_CentroDeportivo.service.IInstructorService;
 import com.utn.API_CentroDeportivo.service.ISportActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/instructors")
@@ -27,7 +30,7 @@ public class InstructorController {
     private ISportActivityService sportActivityService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<InstructorSummaryDTO> getInstructor(@PathVariable Long id){
+    public ResponseEntity<InstructorSummaryDTO> getInstructor(@PathVariable Long id) {
         return instructorService.getInstructorSummaryById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
@@ -45,4 +48,22 @@ public class InstructorController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @GetMapping("/my-activities/{activityId}")
+    public ResponseEntity<SportActivityDetailsDTO> getMyActivityDetails(@PathVariable Long activityId) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Optional<Instructor> optionalInstructor = instructorService.findByUsername(username);
+        Optional<SportActivityDetailsDTO> optionalActivity = sportActivityService.getActivityById(activityId);
+
+        if (optionalInstructor.isPresent() && optionalActivity.isPresent()) {
+            Instructor instructor = optionalInstructor.get();
+            SportActivityDetailsDTO activity = optionalActivity.get();
+
+            if (activity.getInstructorName().equals(instructor.getName())) { // o getFullName()
+                return ResponseEntity.ok(activity);
+            }
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
