1e7f391ff3bae63dcf19a571ebce731e7a32d66b
TomasB <tomasbservant@gmail.com>
2025-06-14 12:53:53 -0300

    agrega endpoint en InstructorController

--- centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/controller/InstructorController.java
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/controller/InstructorController.java
@@ -2,13 +2,16 @@ package com.utn.API_CentroDeportivo.controller;
 
 import com.utn.API_CentroDeportivo.model.dto.response.InstructorDetailsDTO;
 import com.utn.API_CentroDeportivo.model.dto.response.InstructorSummaryDTO;
+import com.utn.API_CentroDeportivo.model.dto.response.MembersDetailsDTO;
 import com.utn.API_CentroDeportivo.model.dto.response.SportActivityDetailsDTO;
 import com.utn.API_CentroDeportivo.model.entity.Instructor;
 import com.utn.API_CentroDeportivo.service.IInstructorService;
+import com.utn.API_CentroDeportivo.service.IMemberService;
 import com.utn.API_CentroDeportivo.service.ISportActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +31,9 @@ public class InstructorController {
 
     @Autowired
     private ISportActivityService sportActivityService;
+
+    @Autowired
+    private IMemberService memberService;
 
     @GetMapping("/{id}")
     public ResponseEntity<InstructorSummaryDTO> getInstructor(@PathVariable Long id) {
@@ -65,5 +71,10 @@ public class InstructorController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+    @PreAuthorize("hasRole('INSTRUCTOR')")
+    @GetMapping("/members/{memberId}")
+    public ResponseEntity<MembersDetailsDTO> getMemberDetails(@PathVariable Long memberId) {
+        return ResponseEntity.ok(memberService.getMemberDetailsById(memberId));
     }
 }
