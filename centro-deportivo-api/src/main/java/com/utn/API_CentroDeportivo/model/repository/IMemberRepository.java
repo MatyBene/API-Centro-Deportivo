3642281dd02dba66971e4782482593826bb10696
TomasB <tomasbservant@gmail.com>
2025-06-14 17:25:33 -0300

    agrega metodo findAll a IMemberRepository

--- centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/model/repository/IMemberRepository.java
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/model/repository/IMemberRepository.java
@@ -1,9 +1,12 @@
 package com.utn.API_CentroDeportivo.model.repository;
 
 import com.utn.API_CentroDeportivo.model.entity.Member;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
 import java.util.Optional;
 
 public interface IMemberRepository extends JpaRepository<Member, Long> {
+    Page<Member> findAll(Pageable pageable);
 }
