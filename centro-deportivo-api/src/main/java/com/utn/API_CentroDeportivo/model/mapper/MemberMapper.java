31dcbb7b07f0787261ce9e782de61551a4cff884
TomasB <tomasbservant@gmail.com>
2025-06-14 12:46:52 -0300

    agrega metodo mapToMemberDetailsDTO a memberMapper

--- centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/model/mapper/MemberMapper.java
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/model/mapper/MemberMapper.java
@@ -1,7 +1,11 @@
 package com.utn.API_CentroDeportivo.model.mapper;
 
 import com.utn.API_CentroDeportivo.model.dto.request.UserRequestDTO;
+import com.utn.API_CentroDeportivo.model.dto.response.EnrollmentDTO;
+import com.utn.API_CentroDeportivo.model.dto.response.MembersDetailsDTO;
 import com.utn.API_CentroDeportivo.model.entity.Member;
+
+import java.util.stream.Collectors;
 
 public class MemberMapper {
 
@@ -14,6 +18,26 @@ public class MemberMapper {
         member.setPhone(memberDTO.getPhone());
         member.setEmail(memberDTO.getEmail());
         return member;
+    }
+    public static MembersDetailsDTO mapToMemberDetailsDTO(Member member) {
+        return MembersDetailsDTO.builder()
+                .name(member.getName())
+                .lastname(member.getLastname())
+                .dni(member.getDni())
+                .birthdate(member.getBirthdate())
+                .phone(member.getPhone())
+                .email(member.getEmail())
+                .username(member.getCredential().getUsername())
+                .role(member.getCredential().getRole())
+                .status(member.getStatus())
+                .enrollments(member.getEnrollments().stream()
+                        .map(enrollment -> EnrollmentDTO.builder()
+                                .activityName(enrollment.getActivity().getName())
+                                .startDate(enrollment.getStartDate())
+                                .endDate(enrollment.getEndDate())
+                                .build())
+                        .collect(Collectors.toList()))
+                .build();
     }
 
 
