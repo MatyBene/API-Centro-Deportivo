a545e5f92d0d82fe43eaa72714c0b0d8bee3f0bf
Matias Benedetti <benedettimaty@gmail.com>
2025-06-14 13:01:21 -0300

    agrega endpoint createUser a AdminController

---
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/controller/AdminController.java
@@ -0,0 +1,25 @@
+package com.utn.API_CentroDeportivo.controller;
+
+import com.utn.API_CentroDeportivo.model.dto.request.UserRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+
+    @Autowired
+    private IAdminService adminService;
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/create-user")
+    public ResponseEntity<String> createUser(@RequestBody UserRequestDTO userDTO) {
+        adminService.createUser(userDTO);
+        return ResponseEntity.ok("Usuario creado correctamente");
+    }
+}
