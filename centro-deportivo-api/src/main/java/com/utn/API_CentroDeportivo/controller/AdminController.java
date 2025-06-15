637ec316c2e9ffeb8eb2d861fa8e46d71e7d2987
Matias Benedetti <benedettimaty@gmail.com>
2025-06-14 23:36:21 -0300

    crea endpoint enrollMemberInActivity en AdminController

--- centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/controller/AdminController.java
+++ centro-deportivo-api/src/main/java/com/utn/API_CentroDeportivo/controller/AdminController.java
@@ -1,14 +1,12 @@
 package com.utn.API_CentroDeportivo.controller;
 
+import com.utn.API_CentroDeportivo.model.dto.request.EnrollmentRequestDTO;
 import com.utn.API_CentroDeportivo.model.dto.request.UserRequestDTO;
 import com.utn.API_CentroDeportivo.service.IAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/admin")
@@ -36,6 +34,13 @@ public class AdminController {
     public ResponseEntity<String> createAdmin(@RequestBody UserRequestDTO userDTO) {
         adminService.createUser(userDTO);
         return ResponseEntity.ok("Admin creado correctamente");
+    }
+
+    @PreAuthorize("hasRole('ADMIN') and (hasAuthority('PERMISSION_SUPER_ADMIN') or hasAuthority('PERMISSION_USER_MANAGER'))")
+    @PostMapping("/enroll-member")
+    public ResponseEntity<String> enrollMemberInActivity(@RequestBody EnrollmentRequestDTO request) {
+        adminService.enrollMemberToActivity(request.getUsername(), request.getActivityId());
+        return ResponseEntity.ok("El socio se inscribió correctamente en la actividad");
     }
 
 }
