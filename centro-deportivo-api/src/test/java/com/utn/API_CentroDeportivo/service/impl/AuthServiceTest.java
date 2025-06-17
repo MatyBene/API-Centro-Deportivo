30a45dc25499270d6bc681489a2bfbb1e6022514
Matias Benedetti <benedettimaty@gmail.com>
2025-06-16 21:21:44 -0300

    agrega test para registerMember

--- centro-deportivo-api/src/test/java/com/utn/API_CentroDeportivo/service/impl/AuthServiceTest.java
+++ centro-deportivo-api/src/test/java/com/utn/API_CentroDeportivo/service/impl/AuthServiceTest.java
@@ -4,17 +4,22 @@ import com.utn.API_CentroDeportivo.config.SecurityConfig;
 import com.utn.API_CentroDeportivo.model.dto.request.MemberRequestDTO;
 import com.utn.API_CentroDeportivo.model.entity.Credential;
 import com.utn.API_CentroDeportivo.model.entity.Member;
+import com.utn.API_CentroDeportivo.model.entity.User;
+import com.utn.API_CentroDeportivo.model.enums.Status;
 import com.utn.API_CentroDeportivo.model.repository.IUserRepository;
 import com.utn.API_CentroDeportivo.service.ICredentialService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AuthServiceTest {
@@ -62,6 +67,25 @@ class AuthServiceTest {
         when(securityConfig.passwordEncoder()).thenReturn(passwordEncoder);
     }
 
+    @Test
+    void registerMember_WhenDataIsValid_ShouldSaveUserWithInactiveStatus(){
+        // Arrange
+        when(userRepository.existsByDni(anyString())).thenReturn(false);
+        when(userRepository.existsByEmail(anyString())).thenReturn(false);
+        when(credentialService.existsByUsername(anyString())).thenReturn(false);
+        when(passwordEncoder.encode(anyString())).thenReturn("encodedPassword");
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+
+        // Act
+        authService.registerMember(memberRequestDTO);
+
+        // Assert
+        verify(userRepository, times(1)).save(userCaptor.capture());
+        User savedUser = userCaptor.getValue();
+        assertTrue(savedUser instanceof Member);
+        assertEquals(Status.INACTIVE, ((Member) savedUser).getStatus());
+        assertEquals("encodedPassword", savedUser.getCredential().getPassword());
+    }
 
 
 }
