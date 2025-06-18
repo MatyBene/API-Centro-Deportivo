321a7821e7fa60869d557ab6544cf695fafed2f9
Matias Benedetti <benedettimaty@gmail.com>
2025-06-18 10:35:18 -0300

    agrupa test con anotacion Nested

--- centro-deportivo-api/src/test/java/com/utn/API_CentroDeportivo/service/impl/CredentialServiceTest.java
+++ centro-deportivo-api/src/test/java/com/utn/API_CentroDeportivo/service/impl/CredentialServiceTest.java
@@ -5,6 +5,7 @@ import com.utn.API_CentroDeportivo.model.entity.Member;
 import com.utn.API_CentroDeportivo.model.entity.User;
 import com.utn.API_CentroDeportivo.model.repository.ICredentialRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -31,7 +32,7 @@ class CredentialServiceTest {
     private final String nonExistentUsername = "nonExistentUser";
     private final Long userId = 1L;
     private final Long credentialId = 1L;
-    private final String name = "Matias";
+    private final String name = "nameTest";
     private final String password = "Password123!";
 
     @BeforeEach
@@ -47,70 +48,79 @@ class CredentialServiceTest {
         credential.setUser(user);
     }
 
-    @Test
-    void loadUserByUsername_WhenUsersExists_ShouldReturnUserDetails() {
-        // Arrange
-        when(credentialRepository.findByUsername(existingUsername)).thenReturn(credential);
+    @Nested
+    class LoadUserByUsernameTests {
+        @Test
+        void whenUsersExists_ShouldReturnUserDetails() {
+            // Arrange
+            when(credentialRepository.findByUsername(existingUsername)).thenReturn(credential);
 
-        // Act
-        UserDetails userDetails = credentialService.loadUserByUsername(existingUsername);
+            // Act
+            UserDetails userDetails = credentialService.loadUserByUsername(existingUsername);
 
-        // Assert
-        assertNotNull(userDetails);
-        assertEquals(existingUsername, userDetails.getUsername());
-        verify(credentialRepository, times(1)).findByUsername(existingUsername);
+            // Assert
+            assertNotNull(userDetails);
+            assertEquals(existingUsername, userDetails.getUsername());
+            verify(credentialRepository, times(1)).findByUsername(existingUsername);
+        }
+
+        @Test
+        void whenUserDoesNotExist_ShouldThrowUsernameNotFoundException() {
+            // Arrange
+            when(credentialRepository.findByUsername(nonExistentUsername)).thenReturn(null);
+
+            // Act & Assert
+            UsernameNotFoundException exception = assertThrows(UsernameNotFoundException.class, () -> {
+                credentialService.loadUserByUsername(nonExistentUsername);
+            });
+            System.out.println(exception.getMessage());
+            assertTrue(exception.getMessage().contains("El nombre de usuario no existe: " + nonExistentUsername));
+        }
     }
 
-    @Test
-    void loadUserByUsername_WhenUserDoesNotExist_ShouldThrowUsernameNotFoundException() {
-        // Arrange
-        when(credentialRepository.findByUsername(nonExistentUsername)).thenReturn(null);
+    @Nested
+    class ExistsByUsernameTests {
+        @Test
+        void whenUsernameExists_ShouldReturnTrue() {
+            // Arrange
+            when(credentialRepository.existsByUsername(existingUsername)).thenReturn(true);
 
-        // Act & Assert
-        UsernameNotFoundException exception = assertThrows(UsernameNotFoundException.class, () -> {
-            credentialService.loadUserByUsername(nonExistentUsername);
-        });
-        System.out.println(exception.getMessage());
-        assertTrue(exception.getMessage().contains("El nombre de usuario no existe: " + nonExistentUsername));
+            // Act
+            boolean result = credentialService.existsByUsername(existingUsername);
+
+            // Assert
+            assertTrue(result);
+            verify(credentialRepository, times(1)).existsByUsername(existingUsername);
+        }
+
+        @Test
+        void whenUsernameDoesNotExist_ShouldReturnFalse() {
+            // Arrange
+            when(credentialRepository.existsByUsername(nonExistentUsername)).thenReturn(false);
+
+            // Act
+            boolean result = credentialService.existsByUsername(nonExistentUsername);
+
+            // Assert
+            assertFalse(result);
+            verify(credentialRepository, times(1)).existsByUsername(nonExistentUsername);
+        }
     }
 
-    @Test
-    void existsByUsername_WhenUsernameExists_ShouldReturnTrue() {
-        // Arrange
-        when(credentialRepository.existsByUsername(existingUsername)).thenReturn(true);
+    @Nested
+    class GetUserByUsernameTests {
+        @Test
+        void whenUserExists_ShouldReturnUserEntity() {
+            // Arrange
+            when(credentialRepository.findByUsername(existingUsername)).thenReturn(credential);
 
-        // Act
-        boolean result = credentialService.existsByUsername(existingUsername);
+            // Act
+            User foundUser = credentialService.getUserByUsername(existingUsername);
 
-        // Assert
-        assertTrue(result);
-        verify(credentialRepository, times(1)).existsByUsername(existingUsername);
-    }
-
-    @Test
-    void existsByUsername_WhenUsernameDoesNotExist_ShouldReturnFalse() {
-        // Arrange
-        when(credentialRepository.existsByUsername(nonExistentUsername)).thenReturn(false);
-
-        // Act
-        boolean result = credentialService.existsByUsername(nonExistentUsername);
-
-        // Assert
-        assertFalse(result);
-        verify(credentialRepository, times(1)).existsByUsername(nonExistentUsername);
-    }
-
-    @Test
-    void getUserByUsername_WhenUserExists_ShouldReturnUserEntity() {
-        // Arrange
-        when(credentialRepository.findByUsername(existingUsername)).thenReturn(credential);
-
-        // Act
-        User foundUser = credentialService.getUserByUsername(existingUsername);
-
-        // Assert
-        assertNotNull(foundUser);
-        assertEquals(user.getId(), foundUser.getId());
-        assertEquals("Matias", foundUser.getName());
+            // Assert
+            assertNotNull(foundUser);
+            assertEquals(user.getId(), foundUser.getId());
+            assertEquals("nameTest", foundUser.getName());
+        }
     }
 }
