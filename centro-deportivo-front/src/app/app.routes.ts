1a47dcdd97cfb03586168c4ebf4251efe75f0c76
Matias Benedetti <benedettimaty@gmail.com>
2025-10-22 13:38:39 -0300

    crea form-page, home-page. agrega rutas en app.routes

--- centro-deportivo-front/src/app/app.routes.ts
+++ centro-deportivo-front/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { FormPage } from './pages/form-page/form-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomePage},
+    {path: 'public/register', component: FormPage}
+];
