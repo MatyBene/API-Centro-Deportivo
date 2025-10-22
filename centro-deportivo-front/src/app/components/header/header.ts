6225c0ce6820df42afc3f1e96c81818880b33ee3
Matias Benedetti <benedettimaty@gmail.com>
2025-10-22 13:49:16 -0300

    modifica app y header

--- centro-deportivo-front/src/app/components/header/header.ts
+++ centro-deportivo-front/src/app/components/header/header.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  
+  constructor(private router: Router){}
+  // constructor(public authService: AuthService, private router: Router){}
 
+  logout(){
+    // this.authService.logout();
+    this.router.navigate(['/']);
+  }
 }
