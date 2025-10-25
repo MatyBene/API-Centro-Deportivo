f2e0f7e604562c7b9ba420789713f1fa9984643d
Matias Benedetti <benedettimaty@gmail.com>
2025-10-24 21:41:30 -0300

    crea time-range-search-box. modifica css, aplica capa oscura a imagen de fondo

---
+++ centro-deportivo-front/src/app/components/time-range-search-box/time-range-search-box.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-range-search-box',
+  imports: [],
+  templateUrl: './time-range-search-box.html',
+  styleUrl: './time-range-search-box.css'
+})
+export class TimeRangeSearchBox {
+
+}
